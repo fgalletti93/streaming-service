e57ad79424543400d048d41bb4533ede5f965ec9
Francesca Galletti <francesca.galletti@pipefy.com>
2022-12-11 20:13:08 -0300

    create button and logic for sign in or out

--- src/components/GoogleAuth.tsx
+++ src/components/GoogleAuth.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
 
 
 const GoogleAuth = (): ReactElement => {
   const [isSignedIn, setIsSignedIn] = useState<Boolean | null>(null)
+  let auth = useRef<any>()
 
   useEffect(() => {
     window.gapi.load('client:auth2', () => {
@@ -11,20 +12,44 @@ const GoogleAuth = (): ReactElement => {
         scope: 'email',
         plugin_name: 'StreaMe'
       }).then(() => {
-        const auth = window.gapi.auth2.getAuthInstance();
-        setIsSignedIn(auth.isSignedIn.get());
+        auth.current = window.gapi.auth2.getAuthInstance();
+        setIsSignedIn(auth.current.isSignedIn.get());
+        return auth.current.isSignedIn.listen(onAuthChange)  //not sure why I needed this return 
       });
     });
 
   }, [])
 
-  const renderAuthButton = () => {
+  const onAuthChange = (): void => {
+    setIsSignedIn(prevState => !prevState)
+  }
+
+  const onSignInClick = (): void => {
+    auth.current.signIn()
+  }
+
+  const onSignOutClick = (): void => {
+    auth.current.signOut()
+  }
+
+
+  const renderAuthButton = (): JSX.Element | null => {
     if(isSignedIn === null) {
-      return <div>I don't think you're signed in</div>
+      return null;
     } else if (isSignedIn) {
-      return <div>Already signed in</div>
+      return (
+        <button onClick={onSignOutClick} className="ui red google button">
+          <i className="google icon"/>
+          Sign Out
+        </button>
+      )
     } else {
-      return <div>Not signed in</div>
+      return (
+        <button onClick={onSignInClick} className="ui red google button">
+          <i className="google icon"/>
+          Sign In with Google
+        </button>
+      )
     }
   }
 
